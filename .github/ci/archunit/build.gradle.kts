5205fbe1f3cb4720faa188f24ce4ab7f6e8ea22b
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-06-20 07:20:34 +0000

    [Gradle] Update Gradle dependencies.

--- .github/ci/archunit/build.gradle.kts
+++ .github/ci/archunit/build.gradle.kts
@@ -37,7 +37,7 @@ sourceSets {
 }
 
 dependencies {
-    implementation("io.valkyrja:valkyrja:26.1.1")
+    implementation("io.valkyrja:valkyrja:26.1.2")
     compileOnly("org.jspecify:jspecify:1.0.0")
     testImplementation("com.tngtech.archunit:archunit-junit5:1.4.2")
     testImplementation("org.junit.jupiter:junit-jupiter:6.1.0")
